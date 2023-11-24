1d6b8c8ccb91afc620e39b935c72822e64fd5cee
muthusubra <muthu.subramanian@digital.justice.gov.uk>
2023-11-24 10:43:08 +0000

    Integration Test

--- crime-validation/src/main/java/uk/gov/justice/laa/crime/validation/exception/CrimeValidationExceptionHandler.java
+++ crime-validation/src/main/java/uk/gov/justice/laa/crime/validation/exception/CrimeValidationExceptionHandler.java
@@ -1,10 +1,12 @@
 package uk.gov.justice.laa.crime.validation.exception;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import uk.gov.justice.laa.crime.commons.tracing.TraceIdHandler;
 import uk.gov.justice.laa.crime.validation.dto.ErrorDTO;
 
 import java.util.List;
@@ -12,17 +14,29 @@ import java.util.stream.Collectors;
 
 
 @RestControllerAdvice
+@RequiredArgsConstructor
 @Slf4j
 public class CrimeValidationExceptionHandler {
+
+    private final TraceIdHandler traceIdHandler;
 
     private static ResponseEntity<ErrorDTO> buildErrorResponse(HttpStatus status, List<String> errorMessage) {
         return new ResponseEntity<>(ErrorDTO.builder().code(status.toString()).messageList(errorMessage).build(), status);
     }
 
+    private static ResponseEntity<ErrorDTO> buildErrorResponseString(HttpStatus status, String message, String traceId) {
+        return new ResponseEntity<>(ErrorDTO.builder().traceId(traceId).code(status.toString()).message(message).build(), status);
+    }
     @ExceptionHandler(CrimeValidationException.class)
     public ResponseEntity<ErrorDTO> handleCrimeValidationException(CrimeValidationException ex) {
         log.error("CrimeValidationException: ", ex);
         return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getExceptionMessage().stream().collect(Collectors.toList()));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorDTO> handleIllegalArgumentException(IllegalArgumentException ex) {
+        log.error("CrimeValidationException: ", ex);
+        return buildErrorResponseString(HttpStatus.BAD_REQUEST, ex.getMessage(), traceIdHandler.getTraceId());
     }
 
 }
--- crime-validation/src/main/java/uk/gov/justice/laa/crime/validation/service/ValidationService.java
+++ crime-validation/src/main/java/uk/gov/justice/laa/crime/validation/service/ValidationService.java
@@ -20,11 +20,11 @@ public class ValidationService {
 
     public Boolean isUserActionValid(@NonNull ApiIsRoleActionValidRequest request) {
         List<String> crimeValidationExceptionList = new ArrayList<>();
-        UserSummaryDTO userSummaryDTO = maatCourtDataService.getUserSummary(request.getUsername());
 
         if (request.getAction() == null && request.getNewWorkReason() == null && request.getSessionId() == null) {
             throw new IllegalArgumentException("Action, New work reason and Session does not exist");
         }
+        UserSummaryDTO userSummaryDTO = maatCourtDataService.getUserSummary(request.getUsername());
 
         if (request.getAction() != null && (userSummaryDTO.getRoleActions() == null
                 || !userSummaryDTO.getRoleActions().contains(request.getAction().getCode()))) {
